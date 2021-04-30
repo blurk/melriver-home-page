8ca9ef04ba1717c71508c4e5350d0ab5ca3ca4f0
blurk <55154720+blurk@users.noreply.github.com>
2021-04-30 21:54:48 +0700

    feat: overlay-top component and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,60 @@
+import gsap from 'gsap';
 import React, { useEffect } from 'react';
 import Banner from './components/Banner';
 import Cases from './components/Cases';
 import Header from './components/Header';
+import IntroOverlay from './components/IntroOverlay';
 import './styles/App.scss';
 
 function App() {
 	useEffect(() => {
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+		gsap.to('body', 0, {
+			css: { visibility: 'visible' }
+		});
+
+		const tl = gsap.timeline();
+
+		tl.from('.line span', 1.8, {
+			y: 100,
+			ease: 'power4.out',
+			delay: 1,
+			skewY: 7,
+			stagger: {
+				amount: 0.3
+			}
+		})
+			.to('.overlay-top', 1.6, {
+				height: 0,
+				ease: 'expo.inOut',
+				stagger: 0.4
+			})
+			.to('.overlay-bottom', 1.6, {
+				width: 0,
+				ease: 'expo.inOut',
+				delay: -0.8,
+				stagger: {
+					amount: 0.4
+				}
+			})
+			.to('.intro-overlay', 0, {
+				css: { display: 'none' }
+			})
+			.from('.case-image img', 1.6, {
+				scale: 1.4,
+				ease: 'expo.inOut',
+				delay: -2,
+				stagger: {
+					amount: 0.4
+				}
+			});
 	}, []);
 
 	return (
 		<div className='App'>
+			<IntroOverlay />
 			<Header />
 			<Banner />
 			<Cases />
---
+++ src/components/IntroOverlay.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function IntroOverlay() {
+	return (
+		<>
+			<div className='intro-overlay'>
+				<div className='top'>
+					<div className='overlay-top'></div>
+					<div className='overlay-top'></div>
+					<div className='overlay-top'></div>
+				</div>
+				<div className='bottom'>
+					<div className='overlay-bottom'></div>
+					<div className='overlay-bottom'></div>
+					<div className='overlay-bottom'></div>
+				</div>
+			</div>
+		</>
+	);
+}
